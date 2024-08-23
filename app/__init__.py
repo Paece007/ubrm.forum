f40c6465ead99a992e3967c484cb997f00936b61
Philipp <p.g.wiederkehr@gmx.at>
2024-08-24 00:59:46 +0200

    changed instance_path to be tmp/instance

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 import logging
+import os
 
 
 from config import Config
@@ -21,13 +22,16 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    # Ensure the instance path exists
+    os.makedirs(app.config['INSTANCE_PATH'], exist_ok=True)
+    app.instance_path = app.config['INSTANCE_PATH']
+
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     csrf.init_app(app)  # Ensure CSRF protection is enabled globally
 
     with app.app_context():
-        from . import models
         db.create_all()
 
     return app
--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    INSTANCE_PATH = os.environ.get('INSTANCE_PATH') or '/tmp/instance'

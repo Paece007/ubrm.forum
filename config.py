29ee1c36fb7bde7d1ae123d6b586005d8e81ff1d
Philipp <p.g.wiederkehr@gmx.at>
2024-08-24 01:25:51 +0200

    correctly reading SQLalchemy_uri from env

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False

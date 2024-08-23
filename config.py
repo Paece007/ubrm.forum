79c04a16b01faf84ec42ddec0c07b18edd3583ad
Philipp <p.g.wiederkehr@gmx.at>
2024-08-23 19:17:02 +0200

    went back to have NO .gitignore file

---
+++ config.py
@@ -0,0 +1,9 @@
+# app/config.py
+import secrets
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///database.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'app', 'static', 'files')

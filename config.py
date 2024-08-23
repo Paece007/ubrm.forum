91bd0a248563411b0ab9fdac714bc3c06edc411e
Philipp <p.g.wiederkehr@gmx.at>
2024-08-24 00:48:33 +0200

    deleted the upload folder

--- config.py
+++ config.py
@@ -6,4 +6,3 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'app', 'static', 'files')

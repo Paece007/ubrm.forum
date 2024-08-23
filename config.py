f4adf91760258006bdd85508ee99b65f2efae101
Philipp <p.g.wiederkehr@gmx.at>
2024-08-23 22:42:50 +0200

    migrated to postgresql, still unsecure

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///database.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://ubrm-forum_owner:TYwS1sBaE3HM@ep-odd-forest-a294zmzw.eu-central-1.aws.neon.tech/ubrm-forum?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'app', 'static', 'files')

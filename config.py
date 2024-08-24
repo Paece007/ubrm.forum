15dc168d47ee0a03ebb68529bf24ecec7b261705
Philipp <p.g.wiederkehr@gmx.at>
2024-08-24 02:05:15 +0200

    updated vercel.json file

--- config.py
+++ config.py
@@ -4,5 +4,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
+    print(f"DATABASE_URL: {os.environ.get('DATABASE_URL')}")
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    print(f"SQLALCHEMY_DATABASE_URI: {SQLALCHEMY_DATABASE_URI}")
     SQLALCHEMY_TRACK_MODIFICATIONS = False

7db5e8a938a5ea46e84f76d515d4c6d42e6ddf7b
Philipp <p.g.wiederkehr@gmx.at>
2024-08-30 13:29:41 +0200

    added 500 error info page

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template# Import the request object
+from flask import Flask, request, render_template, session # Import the request object
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -6,7 +6,6 @@ from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect, CSRFError
 import logging
 import tempfile
-import os
 
 
 from config import Config
@@ -54,6 +53,10 @@ def create_app():
     @app.errorhandler(CSRFError)
     def handle_csrf_error(e):
         return render_template('csrf_error.html', reason=e.description), 400
+    
+    @app.errorhandler(500)
+    def handle_internal_server_error(e):
+        return render_template('500_error.html', error=str(e)), 500
 
     with app.app_context():
         db.create_all()
--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://ubrm-forum_owner:TYwS1sBaE3HM@ep-odd-forest-a294zmzw.eu-central-1.aws.neon.tech/ubrm-forum?sslmode=require'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
     SESSION_COOKIE_SECURE = True
